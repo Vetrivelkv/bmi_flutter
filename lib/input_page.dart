0246a4d4a409432de8df81813098fe2745514b92
Vetri <37167788+Vetrivelkv@users.noreply.github.com>
2022-07-03 18:42:05 +0530

    age and weight RoundIconButton

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -14,6 +14,8 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Gender selectedGender = Gender.none;
   int height = 180;
+  int weight = 65;
+  int age = 25;
 
   @override
   Widget build(BuildContext context) {
@@ -75,25 +77,25 @@ class _InputPageState extends State<InputPage> {
                           style: knumberTextStyle,
                         ),
                         Text('CM', style: klabelTextStyle),
-
                       ],
                     ),
                     SliderTheme(
-                      data:SliderTheme.of(context).copyWith(
-                        thumbColor: Color(0xFFEB1555),
-                        inactiveTrackColor:Color(0xFF8D8E98) ,
-                        overlayColor: Color(0x29EB1555),
-                        thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
-                        overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
-                        activeTrackColor: Colors.white
-                      ) ,
+                      data: SliderTheme.of(context).copyWith(
+                          thumbColor: Color(0xFFEB1555),
+                          inactiveTrackColor: Color(0xFF8D8E98),
+                          overlayColor: Color(0x29EB1555),
+                          thumbShape:
+                              RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                          overlayShape:
+                              RoundSliderOverlayShape(overlayRadius: 30.0),
+                          activeTrackColor: Colors.white),
                       child: Slider(
                         value: height.toDouble(),
                         min: 120.0,
                         max: 220.0,
                         onChanged: (double newValue) {
                           setState(() {
-                            height=newValue.toInt();
+                            height = newValue.toInt();
                           });
                         },
                       ),
@@ -108,13 +110,79 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: Reusable_Card(
                     colour: kactiveCardColor,
-                    cardChild: Column(),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Weight',
+                          style: klabelTextStyle,
+                        ),
+                        Text(weight.toString(), style: knumberTextStyle),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onPress: () {
+                                setState(() {
+                                  weight -= 1;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              onPress: () {
+                                setState(() {
+                                  weight += 1;
+                                });
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: Reusable_Card(
                     colour: kactiveCardColor,
-                    cardChild: Column(),
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Age',
+                          style: klabelTextStyle,
+                        ),
+                        Text(age.toString(), style: knumberTextStyle),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.minus,
+                              onPress: () {
+                                setState(() {
+                                  age -= 1;
+                                });
+                              },
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            RoundIconButton(
+                              icon: FontAwesomeIcons.plus,
+                              onPress: () {
+                                setState(() {
+                                  age += 1;
+                                });
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                 ),
               ],
@@ -127,5 +195,24 @@ class _InputPageState extends State<InputPage> {
             )
           ],
         ));
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  const RoundIconButton({this.icon, this.onPress});
+
+  final IconData? icon;
+  final void Function()? onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      constraints: BoxConstraints.tightFor(width: 56.0, height: 56.0),
+      onPressed: onPress,
+      elevation: 6.0,
+      shape: CircleBorder(),
+      fillColor: Color(0xFF4C4F5E),
+    );
   }
 }
